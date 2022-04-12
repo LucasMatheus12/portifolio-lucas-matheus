ed081155c05bd18ab2d93eb258e1c2fa154f6263
LucasMatheus12 <matheusalexandre012@gmail.com>
2022-04-11 21:54:08 -0300

    Atualizando a sessão projetos

--- arquivo.js
+++ arquivo.js
@@ -28,3 +28,23 @@ window.addEventListener("scroll", function(){
 
 });
 
+
+
+// Carrossel 
+
+
+let count =1; 
+document.getElementById("radio1").checked = true; 
+
+setInterval(function(){
+    nextImage();
+
+}, 3000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1; 
+    }
+    document.getElementById("radio"+count).checked = true; 
+}
